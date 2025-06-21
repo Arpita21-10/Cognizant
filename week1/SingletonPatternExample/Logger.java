d7bb36a51e07a516e327905ef8f25f15cf35d4b8
Arpita21-10 <chatterjeearpita021@gmail.com>
2025-06-21 15:45:58 +0530

    Add files via upload

---
+++ week1/SingletonPatternExample/Logger.java
@@ -0,0 +1,24 @@
+package com.singleton;
+
+public class Logger {
+    
+    private static Logger singleInstance;
+
+ 
+    private Logger() {
+        System.out.println("Logger initialized.");
+    }
+
+
+    public static Logger getInstance() {
+        if (singleInstance == null) {
+            singleInstance = new Logger(); 
+        }
+        return singleInstance;
+    }
+
+
+    public void log(String message) {
+        System.out.println("[LOG]: " + message);
+    }
+}
